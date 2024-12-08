912c683caa1a2bdc9d68a9a3e4a40e5a7df7deed
simonvarey <simonvarey@gmail.com>
2024-12-07 23:32:11 -0500

    Updated hoist-variable-declaration imports example for TS 5.7.2

--- example-transformers/hoist-variable-declaration/transformer.ts
+++ example-transformers/hoist-variable-declaration/transformer.ts
@@ -11,7 +11,7 @@ const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
       return ts.visitEachChild(node, visitor, context);
     };
 
-    return ts.visitNode(sourceFile, visitor);
+    return ts.visitNode(sourceFile, visitor, ts.isSourceFile);
   };
 };
 
