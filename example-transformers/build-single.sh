286caeaae9736b01a5314b741228f31ec4e39f7c
simonvarey <simonvarey@gmail.com>
2024-12-08 23:18:11 -0500

    Reverse temporary changes to build scripts

--- example-transformers/build-single.sh
+++ example-transformers/build-single.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd ./example-transformers/$1 && /c/repos/typescript-transformer-handbook/node_modules/.bin/tspc
+cd ./example-transformers/$1 && tspc
--- example-transformers/build.sh
+++ example-transformers/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd example-transformers && ls -d */ | xargs -I {} bash -c "cd '{}' && /c/repos/typescript-transformer-handbook/node_modules/.bin/tspc"
+cd example-transformers && ls -d */ | xargs -I {} bash -c "cd '{}' && tspc"
